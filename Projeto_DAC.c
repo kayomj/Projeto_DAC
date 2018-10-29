b3b14867bb9eb25d9ead068e74e43d967930ac71
kayomj <kayomj@gmail.com>
2018-10-29 20:15:01 -0300

    Correção das funções sem int Main

--- Projeto_DAC.c
+++ Projeto_DAC.c
@@ -1,40 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-struct aluno{
+struct aluno {
 	char nome[100],ra[8], senha[9];
 };
-typedef aluno Aluno;
+typedef struct aluno Aluno;
 
 
 Aluno *add(char nom[100],char log[8],char sen[9]){
 	Aluno *aux;
-	aux=(Aluno*)malloc(sizeof(Aluno);
-	aux->nome=nom;
-	aux->login=log;
-	aux->senha=sen;
+	aux=(Aluno*)malloc(sizeof(Aluno));
+	strcpy(aux->nome,nom);
+	strcpy(aux->ra,log);
+	strcpy(aux->senha,sen);
 	return aux;
 }
 
 void grava(Aluno *aluno){
 	FILE *op;
 	op=fopen("classe.txt","a");
-	fprintf(op,"%s,%s,%s\n",aluno->nome,aluno->login,aluno->senha);
+	fprintf(op,"%s,%s,%s\n",aluno->nome,aluno->ra,aluno->senha);
 }
 
 int verifica(char log[8], char sen[9]){
 	int cont=0;
+	char c, ra[8];
 	FILE*arq;
-	if(arq=fopen("classe.txt","r")==NULL){
-		printf("PROBLEMAS NA ABERTURA DO ARQUIVO")
-	}
+	arq=fopen("classe.txt","r");
 	do{
-	while(c=fgetc(arq!=',')){
 	cont++;
-	}
-	fgets(arq,7,login);
+	c=fgetc(arq);
+	}while(c!=',');
+	fgets(ra,7,arq);
 	fseek(arq,cont+8,SEEK_SET);	
-	fscanf(arq,"%s",&sen)
+	fscanf(arq,"%s",sen);
+	return 0;
 }
 
 
