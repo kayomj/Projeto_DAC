eb8d0956682e8fe69bf3ea9d6bd15e78c368970c
kayomj <kayomj@gmail.com>
2018-10-29 21:32:01 -0300

    Reestruturação de acordo com o Arquivo word

--- Projeto_DAC.c
+++ Projeto_DAC.c
@@ -2,40 +2,50 @@
 #include<stdlib.h>
 #include<string.h>
 
+//Estrutura de aluno
 struct aluno {
-	char nome[100],ra[8], senha[9];
+	char ra[7], nome[100], login[8], senha[9];
 };
 typedef struct aluno Aluno;
 
-
-Aluno *add(char nom[100],char log[8],char sen[9]){
+//Função que adiciona um aluno novo
+Aluno *Cadastro_aluno(char ra[7], char nome[100], char login[8],char senha[9]){
 	Aluno *aux;
-	aux=(Aluno*)malloc(sizeof(Aluno));
-	strcpy(aux->nome,nom);
-	strcpy(aux->ra,log);
-	strcpy(aux->senha,sen);
+	aux=(Aluno*)malloc(sizeof(Aluno));//Reserva espaço na memória
+	strcpy(aux->ra,ra);
+	strcpy(aux->nome,nome);
+	strcpy(aux->login,login);
+	strcpy(aux->senha,senha);
 	return aux;
 }
 
 void grava(Aluno *aluno){
 	FILE *op;
 	op=fopen("classe.txt","a");
-	fprintf(op,"%s,%s,%s\n",aluno->nome,aluno->ra,aluno->senha);
+	fprintf(op,"%s,%s,%s,%s\n",aluno->ra,aluno->nome,aluno->login,aluno->senha);
 }
 
 int verifica(char log[8], char sen[9]){
-	int cont=0;
-	char c, ra[8];
+	int cont=0, validar_senha, validar_log;
+	char c, ra[8],senha[9];
 	FILE*arq;
 	arq=fopen("classe.txt","r");
 	do{
-	cont++;
-	c=fgetc(arq);
-	}while(c!=',');
-	fgets(ra,7,arq);
-	fseek(arq,cont+8,SEEK_SET);	
-	fscanf(arq,"%s",sen);
-	return 0;
+		fseek(arq,7,SEEK_CUR);//Pula 7 posições
+		do{
+			cont++;
+			c=fgetc(arq);
+		}while(c!=',');//Para na primeira virgula
+		fgets(ra,7,arq);//Lê a variável Login do arquivo
+		fseek(arq,1,SEEK_CUR);//Pula a virgula faltante
+		fscanf(arq,"%s",senha);
+		validar_senha=strcmp(sen,senha);
+		validar_log=strcmp(log,ra);
+		if((validar_log==0 && validar_senha==0)){
+			return 0;
+		}
+	}while(c!=EOF);
+	return 1;
 }
 
 
